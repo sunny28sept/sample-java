700ba9134491eadfaed8fdad66222ce543fa8aff
Sunny <sunny.hemnani@coditas.com>
2022-03-27 21:50:24 +0530

    new changes in my branch

--- src/C.java
+++ src/C.java
@@ -1,6 +1,7 @@
 public class C
 {
     public static void main(String[] args) {
+        System.out.println("My favourite IPL team is KKR.");
         System.out.println("Shreyas Iyer is our new Captain.");
     }
 }
