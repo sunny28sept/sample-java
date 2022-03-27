ea4bea4540a1904e48f4e8b06fcf6128eefcf42a
Sunny <sunny.hemnani@coditas.com>
2022-03-27 21:43:19 +0530

    new changes in master

---
+++ src/A.java
@@ -0,0 +1,6 @@
+public class A {
+    public static void main(String[] args) {
+        System.out.println("My name is Kshitij Hemnani.");
+        System.out.println("My name is Sunny Bele.");
+    }
+}
---
+++ src/B.java
@@ -0,0 +1,7 @@
+public class B {
+    public static void main(String[] args) {
+        System.out.println("India is my country.");
+        System.out.println("I love my country.");
+        System.out.println("We are proud Indians.");
+    }
+}
